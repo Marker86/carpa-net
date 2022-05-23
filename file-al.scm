4c5f8f003b4484ad53a4b2f653326371eb7d444d
Joshua Pc. Hdz <josh@RedSunX04.edu.eu.org>
2022-05-23 18:20:48 -0500

    Added octal permission checking function.

--- file-al.scm
+++ file-al.scm
@@ -1,7 +1,8 @@
 ;;; Define the silly file access control functions we will use later.
 
 ;; Use goated module.
-(use-modules (ice9 rdelim))
+(use-modules (ice-9 rdelim)
+	     (ice-9 streams))
 
 ;; Read file into a string, then return it, returns #f if EOF.
 (define (read-file port)
@@ -26,3 +27,28 @@
 	      ((eof-object? current-element) output-list)
 	    (set! output-list
 		  (append! output-list (list current-element))))))))
+
+;; A cache.
+(define group-cache '())
+
+;; Do what the name of the function says.
+(define (update-group-cache)
+  (setgr)
+  (do ((current-element (getgr) (getgr))
+       (output '()))
+      ((not current-element) (set! group-cache output))
+    (set! output (append! output (list current-element)))))
+
+;; Obtain file permissions from octal integer value.
+(define (check:rwx integer read write exec)
+  (define (nor x y)
+    (not (or x y)))
+
+  (define (the-funny x y) ; Wicked logic needed: #t #t = #t, #f #t = #f, #t #f = #t, #f #f = #t.
+    (or (and x y) (nor x y) (and x (not y))))
+
+  (define (rwx integer read write exec)
+    (list (the-funny (positive? (bit-extract integer 2 3)) read) ; Read bit.
+          (the-funny (positive? (bit-extract integer 1 2)) write) ; Write bit.
+          (the-funny (positive? (bit-extract integer 0 1)) exec))) ; Execute bit.
+  (rwx integer read write exec))
