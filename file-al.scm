d18d2e71f1be59a2743d7e0b21623b346802a734
Joshua Pc. Hdz <jpx0208@gmail.com>
2022-06-17 01:46:03 -0500

    Remove useless junk.

--- file-al.scm
+++ file-al.scm
@@ -1,3 +1,4 @@
+
 ;;; Define the silly file access control functions we will use later.
 
 ;; Use goated module.
@@ -26,68 +27,3 @@
 	      ((eof-object? current-element) output-list)
 	    (set! output-list
 		  (append! output-list (list current-element))))))))
-
-;; A cache.
-(define group-cache '())
-
-;; Do what the name of the function says.
-(define (update-group-cache)
-  (setgr)
-  (do ((current-element (getgr) (getgr))
-       (output '()))
-      ((not current-element) (set! group-cache output))
-    (set! output (append! output (list current-element)))))
-
-;; Obtain file permissions from octal integer value.
-(define (check:rwx integer read write exec)
-  (define (nor x y)
-    (not (or x y)))
-
-  (define (the-funny x y) ; Wicked logic needed: #t #t = #t, #f #t = #f, #t #f = #t, #f #f = #t.
-    (or (and x y) (nor x y) (and x (not y))))
-
-  (define (rwx integer read write exec)
-    (list (the-funny (positive? (bit-extract integer 2 3)) read) ; Read bit.
-          (the-funny (positive? (bit-extract integer 1 2)) write) ; Write bit.
-          (the-funny (positive? (bit-extract integer 0 1)) exec))) ; Execute bit.
-  (rwx integer read write exec))
-
-;; Check if atom is inside a list.
-(define (in-list? atom the-list)
-  (if (null? the-list)
-      #f
-      (let walk-through ((current-element (car the-list))
-                         (rest-list (cdr the-list)))
-        (cond
-         ((equal? atom current-element) #t)
-         ((null? rest-list) #f)
-         (else (walk-through (car rest-list) (cdr rest-list)))))))
-
-;; Query to see user complementary groups names.
-(define (complement-user user)
-  (let ((output-list '()))
-    (let walk-through ((current-element (car group-cache))
-                       (rest-list (cdr group-cache)))
-      (when (in-list? user (group:mem current-element))
-        (set! output-list (append! output-list
-                                   (list (group:name current-element)))))
-      (if (null? rest-list)
-          output-list
-          (walk-through (car rest-list) (cdr rest-list))))))
-
-;; Check if a file is accessible to the user with the given access bits.
-(define (check-file file user read write exec)
-  (define (equally-true listium)
-    (equal? listium (list #t #t #t)))
-
-  (let ((file-stat (stat file))
-        (user-entry (getpwnam user)))
-    (or (and ; Owner permissions.
-         (equal? (passwd:uid user-entry) (stat:uid file-stat))
-         (equally-true (check:rwx (bit-extract (stat:perms file-stat) 6 9) read write exec)))
-        (and ; Group permissions.
-         (or
-          (equal? (passwd:gid user-entry) (stat:gid file-stat)) ; Same group as user.
-          (in-list? (group:name (getgrgid (stat:gid file-stat)))
-                    (complement-user (passwd:name user-entry)))) ; User in group.
-         (equally-true (check:rwx (bit-extract (stat:perms file-stat) 3 6) read write exec))))))
