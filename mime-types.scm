721a00220f21d12ac5916f2178130c5c4287a03c
Joshua Pc. Hdz <jpx0208@gmail.com>
2022-06-27 00:02:25 -0500

    Implemented line scanners in regexp terms.

--- mime-types.scm
+++ mime-types.scm
@@ -8,18 +8,26 @@
 (define *mime-types* (make-hash-table 1000))
 
 ;; Regex we shall use.
-(define name-regex (make-regexp "[A-Za-z/]+"))
-(define white-regex (make-regexp "^[[:blank:]]*$"))
+(define name-regexp (make-regexp "[A-Za-z/\\.]+"))
+(define white-regexp (make-regexp "^[[:blank:]]*$"))
 
-;; Nice utility, should simplify the following parse-line function somewhat
-(define (regexp-exec+substring the-regex the-string . the-flags)
-  (let ((the-output (apply regexp-exec the-regex the-string the-flags)))
-    (if the-output
-        (letrec ((substring-bounds (array-ref the-output 1)) ; I bothered enough to get a substring
-                                                             ; as output too.
-                 (lower-bound (car substring-bounds))
-                 (upper-bound (cdr substring-bounds)))
-          (list (array-ref the-output 0)
-                (array-ref the-output 1)
-                (substring the-string lower-bound upper-bound)))
-        #f))) ; If the regex returned false return false too.
+;; Match a single line & return the substring & the suffix, in the following
+;; structure: (substring suffix), returning #f if there was no match.
+(define (regexp-fetch compiled-regexp the-string)
+  (let ((matched-regexp (regexp-exec compiled-regexp the-string)))
+    (if matched-regexp
+        (list (match:substring matched-regexp)
+              (match:suffix matched-regexp))
+        #f)))
+
+;; Iterate & return a list of the elements that form the string in terms of the provided
+;; regexp.
+(define (regexp-scan compiled-regexp the-string)
+  (let ((the-output (cons '() '()))) ; I must LOOOOOOOOOOOOOP.
+    (let iteration ((initial-fetch (regexp-fetch compiled-regexp the-string)))
+      (if initial-fetch
+          (let ((the-substring (car initial-fetch))
+                (rest-string (caddr initial-fetch)))
+            (list-add! the-output the-substring)
+            (iteration (regexp-fetch compiled-regexp rest-string)))
+          (cdr the-output)))))
