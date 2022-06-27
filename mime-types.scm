a0a4897aa30896f722544fd6639b2cf3d34885d3
Joshua Pc. Hdz <jpx0208@gmail.com>
2022-06-27 04:07:08 -0500

    Implemented regexp filtering & a mapper for value-keys lists.

--- mime-types.scm
+++ mime-types.scm
@@ -8,12 +8,14 @@
 (define *mime-types* (make-hash-table 1000))
 
 ;; Regex we shall use.
-(define name-regexp (make-regexp "[A-Za-z/\\.]+"))
+(define name-regexp (make-regexp "[A-Za-z/\\.-]"))
 (define white-regexp (make-regexp "^[[:blank:]]*$"))
+(define comment-regexp (make-regexp "^#.*"))
 
 ;; Utility we shall use, looping through list on terms of loop is too repetitive, going
 ;; against my doctrine, some other functions are bound to be rewritten on terms of macros &
-;; abstractions, this one is inspired by the common lisp implementation of dolist.
+;; abstractions, this one is inspired by the common lisp implementation of dolist, also it
+;; might be moved to another file.
 (define-syntax dolist
   (λ (x)
     (syntax-case x ()
@@ -41,6 +43,15 @@
                    (the-iterator (car the-rest)
                                  (cdr the-rest))))))))))
 
+;; Pipe but reversed, simpler also.
+(define-syntax compose
+  (λ (x)
+    (syntax-case x ()
+      ((_ input f g)
+       #'(f (g input)))
+      ((_ input f g h ...)
+       #'(f (pipe input g h ...))))))
+
 ;; Match a single line & return the substring & the suffix, in the following
 ;; structure: (substring suffix), returning #f if there was no match.
 (define (regexp-fetch compiled-regexp the-string)
@@ -55,9 +66,30 @@
 (define (regexp-scan compiled-regexp the-string)
   (let ((the-output (cons '() '()))) ; I must LOOOOOOOOOOOOOP.
     (let iteration ((initial-fetch (regexp-fetch compiled-regexp the-string)))
-      (if initial-fetch
+      (when initial-fetch
           (let ((the-substring (car initial-fetch))
                 (rest-string (cadr initial-fetch)))
             (list-add! the-output the-substring)
-            (iteration (regexp-fetch compiled-regexp rest-string)))
-          (cdr the-output)))))
+            (iteration (regexp-fetch compiled-regexp rest-string)))))
+    (if (null? (cdr the-output))
+        #f
+        (cdr the-output))))
+
+;; Filter the list of strings in terms of the regexp object provided.
+(define (regexp-filter compiled-regexp string-list)
+  (filter (λ (x)
+            (regexp-exec compiled-regexp x))
+          string-list))
+
+;; Parse the line
+(define (parse-line line))
+
+;; Shove the matches list in MIME types table following this rule:
+;; (type ext1 ext2 ...) -> (hash-set! hash-table ext1 type) ...
+(define (shove-in-mime-types the-list)
+  (let ((the-extensions (cdr the-list))
+        (the-type (car the-list)))
+    (unless (null? the-extensions)
+      (map (λ (x)
+             (hash-set! *mime-types* x the-type))
+           the-extensions))))
